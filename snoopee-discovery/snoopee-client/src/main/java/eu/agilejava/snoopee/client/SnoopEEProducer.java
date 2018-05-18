5893dcdcdd716398640848dd1ac740268de0941e
Ivar Grimstad <ivar.grimstad@gmail.com>
2018-05-18 14:04:32 +0200

    Clean up old config

--- snoopee-discovery/snoopee-client/src/main/java/eu/agilejava/snoopee/client/SnoopEEProducer.java
+++ snoopee-discovery/snoopee-client/src/main/java/eu/agilejava/snoopee/client/SnoopEEProducer.java
@@ -23,22 +23,14 @@
  */
 package eu.agilejava.snoopee.client;
 
-import com.fasterxml.jackson.dataformat.yaml.snakeyaml.Yaml;
-import com.fasterxml.jackson.dataformat.yaml.snakeyaml.error.YAMLException;
-import eu.agilejava.snoopee.SnoopEEConfigurationException;
-import eu.agilejava.snoopee.SnoopEEExtensionHelper;
 import eu.agilejava.snoopee.annotation.SnoopEE;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
-import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 import javax.inject.Inject;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Optional;
 import java.util.logging.Logger;
 
 /**
@@ -51,15 +43,9 @@ public class SnoopEEProducer {
 
     private static final Logger LOGGER = Logger.getLogger("eu.agilejava.snoopee");
 
-//    private Map<String, Object> snoopeeConfig = Collections.EMPTY_MAP;
-
     @ConfigProperty(name = "snoopeeService", defaultValue = "http://localhost:8081/snoopee-service/")
     @Inject
     private String serviceUrl;
-
-    @ConfigProperty(name ="serviceName", defaultValue = "noname")
-    @Inject
-    private String serviceName;
 
     /**
      * Creates a SnoopEEServiceClient for the named service.
@@ -75,52 +61,10 @@ public class SnoopEEProducer {
         final String applicationName = ip.getAnnotated().getAnnotation(SnoopEE.class).serviceName();
 
         LOGGER.config(() -> "producing " + applicationName);
-
-//        String serviceUrl = "http://" + readProperty("snoopeeService", snoopeeConfig);
         LOGGER.config(() -> "Service URL: " + serviceUrl);
 
         return new SnoopEEServiceClient.Builder(applicationName)
                 .serviceUrl(serviceUrl)
                 .build();
-    }
-
-//    private String readProperty(final String key, Map<String, Object> snoopConfig) {
-//
-//        String property = Optional.ofNullable(System.getProperty(key))
-//                .orElseGet(() -> {
-//                    String envProp = Optional.ofNullable(System.getenv(key))
-//                            .orElseGet(() -> {
-//                                String confProp = Optional.ofNullable(snoopConfig.get(key))
-//                                        .orElseThrow(() -> {
-//                                            return new SnoopEEConfigurationException(key + " must be configured either in snoopee.yml or as env or system property");
-//                                        })
-//                                        .toString();
-//                                return confProp;
-//                            });
-//                    return envProp;
-//                });
-//
-//        return property;
-//    }
-
-    /**
-     * Initializes the producer with the SnoopEE configuration properties.
-     */
-    @PostConstruct
-    private void init() {
-
-//        try {
-//            Yaml yaml = new Yaml();
-//            Map<String, Object> props = (Map<String, Object>) yaml.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("/snoopee.yml"));
-//
-//            snoopeeConfig = (Map<String, Object>) props.get("snoopee");
-
-//            if (!SnoopEEExtensionHelper.isSnoopEnabled()) {
-//                SnoopEEExtensionHelper.setServiceName(readProperty("serviceName", snoopeeConfig));
-//            }
-
-//        } catch (YAMLException e) {
-//            LOGGER.config(() -> "No configuration file. Using env properties.");
-//        }
     }
 }
